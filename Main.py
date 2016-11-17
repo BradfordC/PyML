37af9d0dc5fca4be857127bfc3d7b479b3d9a161
BradfordC <chjabr@gmail.com>
2016-11-16 21:25:09 -0600

    Train on an actual image set

--- Main.py
+++ Main.py
@@ -1,27 +1,66 @@
 import GetInput
 import Network
 import numpy as np
+import time
+import math
 
-#Data = GetInput.LoadAllCategories("D:/Chris/Documents/School/Machine Learning/Project/4Categories", True)
+#data = GetInput.LoadAllCategories("C:\\Users\\Chris\\Dropbox\\MLProjectResources\\4Categories", True)
+data = GetInput.LoadAllCategories("D:\\Chris\\Documents\\School\\Machine Learning\\Project\\4Categories", True)
+np.random.shuffle(data)
 
-inputSize = 2
-outputSize = 1
+firstValidIndex = int(len(data) * .7)
+firstTestIndex = int(len(data) * .85)
 
-net = Network.Network(inputSize, outputSize, [3])
+trainingSet = data[:firstValidIndex]
+validationSet = data[firstValidIndex:firstTestIndex]
+testingSet = data[firstTestIndex:]
 
-inputs = [[0,0],[0,1],[1,0],[1,1]]
-outputs = [[0],[1],[1],[0]]
+print(len(trainingSet))
 
-error = 10
-a = 0
-while error > .01:
-    print(a, end="\t")
-    for i in range(0, len(inputs)):
-        input = np.array(inputs[i])
-        expected = np.array(outputs[i])
+net = Network.Network(len(data[0][0]), len(data[0][1]), [255,255,255], 'ReLU')
+
+groupSize = 250
+groupCount = 0
+testImageCount = 0
+testImageCorrect = 0
+
+testFile = open("C:\\Users\\Chris\\Dropbox\\School\\Machine Learning\\Project\\Checkpoint 5\\TestAccuracy.csv", 'w')
+validFile = open("C:\\Users\\Chris\\Dropbox\\School\\Machine Learning\\Project\\Checkpoint 5\\ValidAccuracy.csv", 'w')
+
+start = time.time()
+for i in range(1):
+    #Train one epoch on training set
+    for image in trainingSet:
+        input = np.array(image[0])
+        expected = np.array(image[1])
         output = net.Feedforwad(input)
         error = net.GetError(expected)
-        print(output[0], end="\t")
-        net.Backprop(expected, .2)
-    print(error)
-    a+=1
+        net.Backprop(expected, .0001)
+
+        testImageCount += 1
+        if np.argmax(expected) == np.argmax(output):
+            testImageCorrect += 1
+
+        if testImageCount == groupSize:
+            print(str(i) + " : " + str(groupCount))
+            groupCount += 1
+            groupAccuracy = testImageCorrect / groupSize
+            testFile.write(str(time.time() - start) + ',' + str(groupAccuracy) + '\n')
+            testImageCount = 0
+            testImageCorrect = 0
+
+    #Print validation accuracy after this epoch
+    validImageCorrect = 0
+    for image in validationSet:
+        input = np.array(image[0])
+        expected = np.array(image[1])
+        output = net.Feedforwad(input)
+
+        if np.argmax(expected) == np.argmax(output):
+            validImageCorrect +=1
+    validationAccuracy = validImageCorrect / len(validationSet)
+    validFile.write(str(time.time() - start) + ',' + str(validationAccuracy) + '\n')
+
+
+end = time.time()
+print(str(end - start) + ' seconds.')
